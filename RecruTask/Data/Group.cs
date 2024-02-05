c7376a23a6079c90ec554be7b048e8ebe9226447
KrycuX <Tomekkrynicki30@gmail.com>
2024-02-05 21:01:10 +0100

    Dodaj pliki projektów.

---
+++ RecruTask/Data/Group.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazor;
+using static MudBlazor.Icons.Material;
+
+namespace RecruTask.Data;
+
+public class Group
+{
+    public int Id { get; set; }
+    public string Name { get; set; }=string.Empty;
+    public string Icon { get; set; } = Filled.StarBorder;
+}
